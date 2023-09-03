de0b7877d9e673099c010b852bb808b8a9b5d914
Alex Howard <thezanke@gmail.com>
2023-09-03 11:09:20 -0400

    better logging

--- src/ph8.py
+++ src/ph8.py
@@ -32,8 +32,7 @@ async def on_message(message: discord.Message):
         return
 
     if config.debug_mode:
-        print(f"Received message: {message.content}")
-
+        print(f"Received message:\n  {message.author.name}: {message.content}")
 
     messages = [message]
 
@@ -42,30 +41,41 @@ async def on_message(message: discord.Message):
 
     messages.reverse()
 
-    if config.debug_mode:
-        print(f"Message chain: {messages}")
-
     completion_messages = list(map(map_discord_message_to_openai_input, messages))
+
+    if config.debug_mode:
+        message_chain = "\n".join(
+            ["  " + str(message) for message in completion_messages]
+        )
+        print(f"Message chain:\n{message_chain}")
+
     result = await openai.get_response(completion_messages)
     response = cast(dict, result)["choices"][0]["message"]
 
     if response is None:
         return
-    
+
     if config.debug_mode:
-        print(f"OpenAI Response: {response.content}")
-    
+        print(f"OpenAI Response:\n  {response.content}")
+
     await message.reply(response.content)
 
+
 async def get_reply_chain(message: discord.Message):
+    channel = message.channel
     replies = []
     current_message = message
 
-    while current_message.reference is not None and current_message.reference.message_id is not None:
-        current_message = await current_message.channel.fetch_message(current_message.reference.message_id)
+    while reference_id := get_reference_id(current_message):
+        current_message = await channel.fetch_message(reference_id)
         replies.append(current_message)
 
     return replies
+
+
+def get_reference_id(message: discord.Message):
+    return message.reference and message.reference.message_id
+
 
 def map_discord_message_to_openai_input(message: discord.Message):
     completion_message = {
@@ -80,6 +90,7 @@ def map_discord_message_to_openai_input(message: discord.Message):
 
     return completion_message
 
+
 async def determine_if_reply(message: discord.Message):
     if message.reference is None:
         return False
@@ -93,7 +104,8 @@ async def determine_if_reply(message: discord.Message):
 
     if original_message is None:
         return False
-    
+
     return original_message.author == client.user
+
 
 client.run(config.discord["token"])
