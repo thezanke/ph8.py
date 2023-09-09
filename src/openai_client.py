1266e1b0a1285cb392b5d7d6fe40e258dc15968d
Alex Howard <thezanke@gmail.com>
2023-09-09 09:47:07 -0400

    working on function to read url contents

--- src/openai_client.py
+++ src/openai_client.py
@@ -1,10 +1,41 @@
 from types import SimpleNamespace
 from typing import cast
-import discord
+from bs4 import BeautifulSoup
+import requests
 import openai
 import config
 
 openai.api_key = config.openai["api_key"]
+
+
+functions = [
+    {
+        "name": "get_content_from_url",
+        "description": "Get the text content from a URL",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "url": {
+                    "type": "string",
+                    "description": "The URL to get the content from",
+                },
+            },
+            "required": ["url"],
+        },
+    }
+]
+
+
+async def get_content_from_url(params: dict[str, str]):
+    url = params["url"]
+    respone = requests.get(url)
+    soup = BeautifulSoup(respone.content, "html.parser")
+    return soup.text
+
+
+func_register = {
+    "get_content_from_url": get_content_from_url,
+}
 
 
 async def get_response(
@@ -20,12 +51,20 @@ async def get_response(
         messages=messages,
         model=model,
         temperature=temperature,
+        functions=functions,
     )
 
     response_message = cast(dict, response)["choices"][0]["message"]
 
     if config.debug_mode:
         print(f"OpenAI Response: {response_message}")
+
+    if response_message["function_call"] is not None:
+        function_name = response_message["function_call"]["function"]
+        function_params = response_message["function_call"]["parameters"]
+        function = func_register[function_name]
+        function_response = await function(function_params)
+        fn_response_message = { "content": function_response, "" }
 
     return response_message
 
