5b247234cd9e4922f9e076fc438da866389b69d3
Alex Howard <thezanke@gmail.com>
2023-10-24 13:29:26 -0400

    feat: reply-only mode is working

--- ph8/main.py
+++ ph8/main.py
@@ -1,16 +1,24 @@
-import discord
 import ph8.discord
 import ph8.config
 import ph8.ai.chat
 
 
-async def handle_discord_message(message: discord.Message):
-    print(f"Received message: {message.content}")
-    response = ph8.ai.chat.chain.invoke(message.content)
+async def handle_message(details: ph8.discord.MessageDetails):
+    if not details.should_respond:
+        return
+
+    print(f"Received message: {details.message.content}")
+
+    response = ph8.ai.chat.chain.invoke(details.message.content)
     print(f"Response: {response}")
-    await message.reply(response)
+
+    await details.message.reply(response)
 
 
 def init():
-    ph8.discord.add_message_handler(handle_discord_message)
+    ph8.discord.add_message_handler(handle_message)
     ph8.discord.client.run(ph8.config.discord["token"])
+
+
+if __name__ == "__main__":
+    init()
