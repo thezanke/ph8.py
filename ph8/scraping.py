fa99b7f3e7a00cebab11ad50f9cb509bafcc9353
Alex Howard <thezanke@gmail.com>
2023-11-12 12:44:46 -0500

    small refactor

--- ph8/scraping.py
+++ ph8/scraping.py
@@ -10,17 +10,6 @@ logger = logging.getLogger(__name__)
 def create_chrome_options() -> Options:
     chrome_options = Options()
 
-    # Disable the loading of images
-    chrome_prefs = {}
-    chrome_options.experimental_options["prefs"] = chrome_prefs
-    chrome_prefs["profile.default_content_settings"] = {"images": 2}
-
-    # Run headless
-    chrome_options.add_argument("--headless")
-
-    # Disable shared memory
-    chrome_options.add_argument("--disable-dev-shm-usage")
-
     # Disable sandbox if running as root (Docker)
     if ph8.config.chrome.disable_sandbox:
         logger.info("Running as root, adding --no-sandbox to chrome options")
@@ -29,6 +18,21 @@ def create_chrome_options() -> Options:
     # Set the user agent
     user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
     chrome_options.add_argument(f"user-agent={user_agent}")
+
+    # Run headless
+    chrome_options.add_argument("--headless")
+
+    # Disable shared memory
+    chrome_options.add_argument("--disable-dev-shm-usage")
+
+    # Disable the loading of images
+    chrome_options.add_experimental_option(
+        "prefs",
+        {
+            "profile.default_content_settings.images": 2,
+            "profile.managed_default_content_settings.images": 2,
+        },
+    )
 
     return chrome_options
 
