e8b40a6da4b89de97b6ddb1cd9648748b9fed753
Alex Howard <thezanke@gmail.com>
2023-11-18 11:54:54 -0500

    WIP

--- ph8/discord.py
+++ ph8/discord.py
@@ -9,7 +9,6 @@ class DiscordBot(commands.Bot):
     def __init__(self):
         super().__init__(
             command_prefix="$ph8 ",
-            help_command=None,
             intents=Intents.all(),
             guild_subscriptions=True,
             owner_id=ph8.config.discord.owner_id,
@@ -17,3 +16,4 @@ class DiscordBot(commands.Bot):
 
     async def setup_hook(self):
         await self.load_extension("ph8.conversation")
+        await self.load_extension("ph8.preferences")
---
+++ ph8/preferences.py
@@ -0,0 +1,77 @@
+from logging import getLogger
+from types import SimpleNamespace
+from typing import TypedDict
+import discord
+from discord.ext import commands
+import openai
+import json
+
+logger = getLogger(__name__)
+
+
+class ModelsResponseModel(TypedDict):
+    id: str
+    object: str
+    created: int
+    owned_by: str
+
+
+class ModelsResponse(TypedDict):
+    object: str
+    data: list[ModelsResponseModel]
+
+
+def is_owner():
+    async def predicate(ctx):
+        return ctx.author.id == ctx.bot.owner_id
+
+    return commands.check(predicate)
+
+
+def get_reference_id(message: discord.Message):
+    return message.reference and message.reference.message_id
+
+
+def parse_models_response(res: ModelsResponse):
+    res["data"].sort(key=lambda x: x["created"], reverse=True)
+
+    models = []
+
+    for model in res["data"]:
+        if not model["object"] == "model":
+            continue
+        if not model["id"].startswith("gpt-"):
+            continue
+        models.append(model["id"])
+
+    return models
+
+
+class PreferencesCog(commands.Cog, name="Preferences"):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    async def cog_load(self):
+        models = await self.__get_models()
+        self.model.help = (
+            "Sets the model to use for the bot. Available models:" + "\n".join(models)
+        )
+
+    async def __get_models(self):
+        response: ModelsResponse = openai.Model.list()  # type: ignore
+        return parse_models_response(response)
+
+    @commands.group()
+    async def set(self, ctx: commands.Context):
+        """Sets user preferences"""
+        if ctx.invoked_subcommand is None:
+            await ctx.send_help(ctx.command)
+
+    @set.command()
+    async def model(self, ctx: commands.Context, value: str | None = None):
+        if value is None:
+            await ctx.send_help(ctx.command)
+
+
+async def setup(bot: commands.Bot):
+    await bot.add_cog(PreferencesCog(bot))
