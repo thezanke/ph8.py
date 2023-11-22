2fb9cddac7b2974a39e3a7c63a1530d601559e68
Alex Howard <thezanke@gmail.com>
2023-11-21 23:06:08 -0500

    what in the world are these migrations

--- ph8/db/models/__init__.py
+++ ph8/db/models/__init__.py
@@ -1,5 +1,7 @@
 from .base import Base as _Base
 from .user import User as _User
+from .preference import Preference as _Preference
 
 Base = _Base
 User = _User
+Preference = _Preference
--- ph8/db/scripts/migrate.py
+++ ph8/db/scripts/migrate.py
@@ -2,6 +2,7 @@ from ph8.db import engine, models
 
 
 def migrate():
+    models.Base.metadata.drop_all(engine)
     models.Base.metadata.create_all(engine)
 
 
